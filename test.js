151d831aaa95edb0f427795b5273e5f232c83fdb
babayaga <140975585+deathrexholo@users.noreply.github.com>
2024-12-17 03:04:30 -0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+console.log("hellow")
+const accountId = 1324;
+let accountEmail = "dj@gmail.com";
+var accountPassword = "fuckyi";
+let accountName = "dhananjay";
+accountCity = "bns";
+
+let accountState;
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+accountEmail = "monu2@gmail.com";
+accountPassword = "hellow";
+accountCity = "banaras";
+accountName = "monu";
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
