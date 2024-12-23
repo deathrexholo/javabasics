63b665430a0348edf990fd60c6e94d88b0df428f
babayaga <140975585+deathrexholo@users.noreply.github.com>
2024-12-23 05:05:14 -0800

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,11 @@
+
+"use strict"; // treat all JS code as newer version
+let name = "dj"
+let isLoggedin = true
+let age = 24
+let address = "vedvyaspuri , phase 1 , meerut bahgpat road, new delhi"
+let state
+console.log(typeof address);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedin);
